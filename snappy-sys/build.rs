c009f0f0b841818573c7fb29a79d71aa6e348ed0
André Silva <andre.beat@gmail.com>
2018-06-29 00:51:55 +0100

    use cmake to build snappy-sys

--- snappy-sys/build.rs
+++ snappy-sys/build.rs
@@ -1,22 +1,32 @@
-extern crate cc;
+extern crate cmake;
+
+use std::env;
+
+use cmake::Config;
 
 fn main() {
-	let mut snappy_config = cc::Build::new();
-	snappy_config.include("snappy/");
-	snappy_config.include(".");
+	let out = Config::new("snappy")
+		.define("CMAKE_VERBOSE_MAKEFILE", "ON")
+		.build_target("snappy")
+		.build();
 
-	snappy_config.define("NDEBUG", Some("1"));
+	let mut build = out.join("build");
 
-	if !cfg!(target_env = "msvc") {
-		snappy_config.flag("-std=c++11");
-	} else {
-		snappy_config.flag("-EHsc");
+	if cfg!(target_os = "windows") {
+		let profile = match &*env::var("PROFILE").unwrap_or("debug".to_owned()) {
+			"bench" | "release" => "Release",
+			_ => "Debug",
+		};
+		build = build.join(profile);
 	}
 
-	snappy_config.file("snappy/snappy.cc");
-	snappy_config.file("snappy/snappy-sinksource.cc");
-	snappy_config.file("snappy/snappy-c.cc");
-	snappy_config.cpp(true);
-	snappy_config.compile("libsnappy.a");
-}
+	println!("cargo:rustc-link-search=native={}", build.display());
+	println!("cargo:rustc-link-lib=static=snappy");
 
+	// https://github.com/alexcrichton/cc-rs/blob/ca70fd32c10f8cea805700e944f3a8d1f97d96d4/src/lib.rs#L891
+	if cfg!(any(target_os = "macos", target_os = "freebsd", target_os = "openbsd")) {
+		println!("cargo:rustc-link-lib=c++");
+	} else if cfg!(not(target_env = "msvc")) {
+		println!("cargo:rustc-link-lib=stdc++");
+	}
+}

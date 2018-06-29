02fed5bc9b66000dddec34a4017a093c890a0801
André Silva <andre.beat@gmail.com>
2018-06-29 09:57:51 +0100

    use vec::new since it doesnt allocate eagerly

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,7 @@ pub fn decompressed_len(compressed: &[u8]) -> Result<usize, InvalidInput> {
 
 /// Compress a buffer using snappy.
 pub fn compress(input: &[u8]) -> Vec<u8> {
-	let mut buf = Vec::with_capacity(0);
+	let mut buf = Vec::new();
 	let size = compress_into(input, &mut buf);
 	buf.truncate(size);
 	buf

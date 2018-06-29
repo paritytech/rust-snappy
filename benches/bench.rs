94212b5cfdbac5d383730916e05fb1507eb0238c
André Silva <andre.beat@gmail.com>
2018-06-29 09:55:54 +0100

    fix whitespace in bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,14 +6,14 @@ extern crate snappy;
 
 #[cfg(test)]
 mod tests {
-    use test::Bencher;
+	use test::Bencher;
 	use rand::prelude::*;
 	use snappy;
 
 	const INPUT_SIZE: usize = 1 << 19;
 
-    #[bench]
-    fn bench_compress_decompress(b: &mut Bencher) {
+	#[bench]
+	fn bench_compress_decompress(b: &mut Bencher) {
 		let mut rng = StdRng::from_seed([0u8; 32]);
 		let mut input = [0u8; INPUT_SIZE];
 		rng.fill(&mut input[..]);
@@ -21,9 +21,9 @@ mod tests {
 		let mut compressed = Vec::with_capacity(INPUT_SIZE);
 		let mut decompressed = Vec::with_capacity(INPUT_SIZE);
 
-        b.iter(|| {
+		b.iter(|| {
 			let size = snappy::compress_into(&input, &mut compressed);
 			let _ = snappy::decompress_into(&compressed[..size], &mut decompressed);
-        });
-    }
+		});
+	}
 }

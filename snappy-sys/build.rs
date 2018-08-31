e36ea410d73709d6eb3cf49a9ec2c6f5cff77db0
André Silva <andre.beat@gmail.com>
2018-08-31 15:44:02 +0100

    fix check for target_os android in snappy-sys build

--- snappy-sys/build.rs
+++ snappy-sys/build.rs
@@ -33,10 +33,14 @@ fn main() {
 	println!("cargo:rustc-link-lib=static=snappy");
 	println!("cargo:include={}", build.display());
 
+    // NOTE: the cfg! macro doesn't work when cross-compiling, it would return values for the host
+    let target_os = env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS is set by cargo.");
+    let target_android = target_os.contains("android");
+
 	// https://github.com/alexcrichton/cc-rs/blob/ca70fd32c10f8cea805700e944f3a8d1f97d96d4/src/lib.rs#L891
 	if cfg!(any(target_os = "macos", target_os = "freebsd", target_os = "openbsd")) {
 		println!("cargo:rustc-link-lib=c++");
-	} else if cfg!(not(any(target_env = "msvc", target_os = "android"))) {
+	} else if cfg!(not(target_env = "msvc")) && !target_android {
 		println!("cargo:rustc-link-lib=stdc++");
 	}
 }
